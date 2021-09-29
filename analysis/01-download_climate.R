72a6e004bc35742426107a1d053c8b98095963b6
David LeBauer <dlebauer@arizona.edu>
2021-09-29 13:57:06 -0700

    clean up download and combine masks scripts

--- analysis/01-download_climate.R
+++ analysis/01-download_climate.R
@@ -1,10 +1,20 @@
 vars <- c('tmin','ppt', 'srad')
-URL <- "http://thredds.northwestknowledge.net:8080/thredds/fileServer/TERRACLIMATE_ALL/summaries/TerraClimate19611990_VAR.nc"
+climates <- c('19812010', '2C', '4C')
+URL <- "http://thredds.northwestknowledge.net:8080/thredds/fileServer/TERRACLIMATE_ALL/summaries/"
 for(var in vars){
-  varurl <- gsub('VAR', var, URL)
-  destfile <- paste0('data/raw_data/TerraClimate19812010_',var,'.nc')
-#  if(!file.exists(destfile)){
-    download.file(varurl,
-                  destfile = destfile)
-#  }
+  for(climate in climates){
+    filename <- paste0('TerraClimate', climate, '_',var,'.nc')
+    varurl <- paste0(URL, filename)
+    destfile <- file.path('data/raw_data/', filename)
+
+    if(!(var == 'srad' & climate %in% c('2C', '4C'))){
+       # srad doesn't change w/ climate
+      if(!file.exists(destfile)){
+        download.file(varurl,
+                      destfile = destfile)
+      }
+
+    }
+  }
+
 }
